042c509f9f2eb887b8347cfca4cc8a54c35e75e4
akanksh <akanksh@12356441-8846-4a2e-b694-8675b88add0f>
2010-10-21 18:45:18 +0000

    Cleanup and added a profile run. Added a hiResTimer

--- yog_main.cpp
+++ yog_main.cpp
@@ -9,6 +9,33 @@
 #include "move_gen.hpp"
 #include "hiResTime.hpp"
 
+void RunPerft(const std::string& fen, int depth, int iters, ui64 value)
+{
+	Show<ShowTypes::Console>::Op(fen);
+	Bitboard b = BitboardFromFen(fen);
+	aks::time::HighResStopWatch w;
+	w.startTimer();
+	ui64 sum=0;
+	w.startTimer();
+	for(unsigned int i=0; i<iters; ++i)
+	{
+		sum += Perft(b,depth);
+	}
+	w.stopTimer();
+	std::string dpt = to_string(depth);
+	std::string num = to_string(sum/iters);
+	std::string avg = to_string(sum/w.getElapsedTime());
+	Show<ShowTypes::Console>::Op(std::string("Depth: ")+dpt);
+    Show<ShowTypes::Console>::Op(num);
+    Show<ShowTypes::Console>::Op((unsigned)value);
+	Show<ShowTypes::Console>::Op(w.getElapsedTime()/iters);
+	Show<ShowTypes::Console>::Op(avg);
+	if((sum/iters) != value)
+		Show<ShowTypes::Console>::Op("XXXXXXXXXXXXXXXXXXXXXXXXX");
+	Show<ShowTypes::Console>::Op("----------------------");
+
+}
+
 int main()
 {
     //Bitboard b;
@@ -63,170 +90,67 @@ int main()
     //Show<ShowTypes::Console>::Op(b);
 
 	init_all();
+	
+	bool RunPerfts = true;
 
-	//for(unsigned i=0; i<64; ++i)
-	//{
-	//	Show<ShowTypes::Console>::Op(lookup::direction_attacks[Dir::SE][i]);
-	//	Show<ShowTypes::Console>::Op("--------");
-	//}
-
-	aks::time::HighResStopWatch w;
-
-	bool RunPerfts = false;
-
-	//Show<ShowTypes::Console>::Op( QueenAttacks((Constants::file_d|Constants::rank_5), Sq::g2) );
-
-	w.startTimer();
 	if(RunPerfts)
   {
     const std::string fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    Bitboard b = BitboardFromFen(fen);
-
-    Show<ShowTypes::Console>::Op(Perft(b,1));
-    Show<ShowTypes::Console>::Op("20");
-    Show<ShowTypes::Console>::Op(Perft(b,2));
-    Show<ShowTypes::Console>::Op("400");
-    Show<ShowTypes::Console>::Op(Perft(b,3));
-    Show<ShowTypes::Console>::Op("8902");
-    Show<ShowTypes::Console>::Op(Perft(b,4));
-    Show<ShowTypes::Console>::Op("197281");
-    Show<ShowTypes::Console>::Op(Perft(b,5));
-    Show<ShowTypes::Console>::Op("4865609");
-    Show<ShowTypes::Console>::Op(Perft(b,6));
-    Show<ShowTypes::Console>::Op("119060324");
+	RunPerft(fen, 1, 10000, 20);
+	RunPerft(fen, 2, 1000, 400);
+	RunPerft(fen, 3, 500, 8902);
+	RunPerft(fen, 4, 10, 197281);
+	RunPerft(fen, 5, 1, 4865609);
+	//RunPerft(fen, 6, 1, 119060324);
   }
-	w.stopTimer();
-	Show<ShowTypes::Console>::Op(w.getElapsedTime());
-  
-	w.startTimer();
   if(RunPerfts)
   {
     const std::string fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
-	
-		//const std::string fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R4K1R b kq - 1 1");
-	//const std::string fen("4k3/8/8/8/8/8/4B3/R3K2R b KQ - 0 1");
-	
-    Bitboard b = BitboardFromFen(fen);
-
-
-
-	//std::vector<std::pair<move, int> > div = Divide(b,4);
-	//int sum=0;
-	//for(int i=0; i<div.size(); ++i)
-	//{
-	//	Show<ShowTypes::Console>::Op(div[i].first);
-	//	Show<ShowTypes::Console>::Op(div[i].second);
-	//	sum += div[i].second;
-	//}
-	//Show<ShowTypes::Console>::Op(sum);
-
-	Show<ShowTypes::Console>::Op(Perft(b,1));
-    Show<ShowTypes::Console>::Op("48");
-	Show<ShowTypes::Console>::Op(Perft(b,2));
-    Show<ShowTypes::Console>::Op("2039");
-    Show<ShowTypes::Console>::Op(Perft(b,3));
-    Show<ShowTypes::Console>::Op("97862");
-	Show<ShowTypes::Console>::Op(Perft(b,4));
-    Show<ShowTypes::Console>::Op("4085603");
-	Show<ShowTypes::Console>::Op(Perft(b,5));
-    Show<ShowTypes::Console>::Op(" 193690690");
-	Show<ShowTypes::Console>::Op(Perft(b,6));
-    Show<ShowTypes::Console>::Op("8031647685");
+	RunPerft(fen, 1, 10000, 48);
+	RunPerft(fen, 2, 1000, 2039);
+	RunPerft(fen, 3, 500, 97862);
+	RunPerft(fen, 4, 10, 4085603);
+	RunPerft(fen, 5, 1, 193690690);
+	//RunPerft(fen, 6, 1, 8031647685);
 
   }
-  w.stopTimer();
-  Show<ShowTypes::Console>::Op(w.getElapsedTime());
-
-  w.startTimer();
   if(RunPerfts)
   {
     const std::string fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1");
-    Bitboard b = BitboardFromFen(fen);
-
-    Show<ShowTypes::Console>::Op(Perft(b,1));
-    Show<ShowTypes::Console>::Op("14");
-    Show<ShowTypes::Console>::Op(Perft(b,2));
-    Show<ShowTypes::Console>::Op("191");
-    Show<ShowTypes::Console>::Op(Perft(b,3));
-    Show<ShowTypes::Console>::Op("2812");
-    Show<ShowTypes::Console>::Op(Perft(b,4));
-    Show<ShowTypes::Console>::Op("43238");
-    Show<ShowTypes::Console>::Op(Perft(b,5));
-    Show<ShowTypes::Console>::Op("674624");
-    Show<ShowTypes::Console>::Op(Perft(b,6));
-    Show<ShowTypes::Console>::Op("11030083");
+	RunPerft(fen, 1, 10000, 14);
+	RunPerft(fen, 2, 1000, 191);
+	RunPerft(fen, 3, 500, 2812);
+	RunPerft(fen, 4, 10, 43238);
+	RunPerft(fen, 5, 1, 674624);
+	//RunPerft(fen, 6, 1, 11030083);
   }
-  w.stopTimer();
-  Show<ShowTypes::Console>::Op(w.getElapsedTime());
-
-  w.startTimer();
-    if(RunPerfts)
+  if(RunPerfts)
   {
     const std::string fen("8/3K4/2p5/p2b2r1/5k2/8/8/1q6 b - - 1 67");
-    Bitboard b = BitboardFromFen(fen);
-
-    Show<ShowTypes::Console>::Op(Perft(b,1));
-    Show<ShowTypes::Console>::Op("50");
-    Show<ShowTypes::Console>::Op(Perft(b,2));
-    Show<ShowTypes::Console>::Op("279");
+	RunPerft(fen, 1, 10000, 50);
+	RunPerft(fen, 2, 1000, 279);
   }
-   w.stopTimer();
-  Show<ShowTypes::Console>::Op(w.getElapsedTime());
-
-  w.startTimer();    
     if(RunPerfts)
   {
     const std::string fen("n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1");
-    Bitboard b = BitboardFromFen(fen);
 
-    //std::vector<move> mvsp = GeneratePseudoLegalMoves(b);
-    //std::vector<move> mvsl = GenerateLegalMoves(b);
-    //std::vector<move> mvsi = GenerateIllegalMoves(b);
-    //std::vector<std::pair<move,int> >div = Divide(b,2);
-    Show<ShowTypes::Console>::Op(Perft(b,1));
-    Show<ShowTypes::Console>::Op("24");
-    Show<ShowTypes::Console>::Op(Perft(b,2));
-    Show<ShowTypes::Console>::Op("496");
-    Show<ShowTypes::Console>::Op(Perft(b,3));
-    Show<ShowTypes::Console>::Op("9483");
-    Show<ShowTypes::Console>::Op(Perft(b,4));
-    Show<ShowTypes::Console>::Op("182838");
-    Show<ShowTypes::Console>::Op(Perft(b,5));
-    Show<ShowTypes::Console>::Op("3605103");
-    Show<ShowTypes::Console>::Op(Perft(b,6));
-    Show<ShowTypes::Console>::Op("71179139");
+	RunPerft(fen, 1, 10000, 24);
+	RunPerft(fen, 2, 1000, 496);
+	RunPerft(fen, 3, 500, 9483);
+	RunPerft(fen, 4, 10, 182838);
+	RunPerft(fen, 5, 1, 3605103);
+	//RunPerft(fen, 6, 1, 71179139);
   }
-  w.stopTimer();
-  Show<ShowTypes::Console>::Op(w.getElapsedTime());
-
-  w.startTimer();
   if(RunPerfts)
   {
     const std::string fen("rnbqkb1r/ppppp1pp/7n/4Pp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 0 3");
-    Bitboard b = BitboardFromFen(fen);
-
-    Show<ShowTypes::Console>::Op(Perft(b,5));
-    Show<ShowTypes::Console>::Op("11139762");
+	RunPerft(fen, 5, 1, 11139762);
   }
-  w.stopTimer();
-  Show<ShowTypes::Console>::Op(w.getElapsedTime());
-
-  w.startTimer();
     if(RunPerfts)
   {
     const std::string fen("8/7p/p5pb/4k3/P1pPn3/8/P5PP/1rB2RK1 b - d3 0 28");
-    Bitboard b = BitboardFromFen(fen);
-
-    Show<ShowTypes::Console>::Op(Perft(b,6));
-    Show<ShowTypes::Console>::Op("38633283");
+	//RunPerft(fen, 6, 1, 38633283);
   }
-  w.stopTimer();
-  Show<ShowTypes::Console>::Op(w.getElapsedTime());
-
-	//for(int i=0; i<mvs.size(); ++i)
-	//{
-
-	//}
 
 
     return 0;
