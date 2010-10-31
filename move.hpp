496265f40f1cd48bde31becedba51d64060a370e
akanksh <akanksh@12356441-8846-4a2e-b694-8675b88add0f>
2010-10-31 20:09:50 +0000

    added move_packed

--- move.hpp
+++ move.hpp
@@ -4,6 +4,8 @@
 #include "enums.hpp"
 #include "constants.hpp"
 
+struct move_packed;
+
 struct move
 {
 	PieceType::EPieceType piece;
@@ -42,8 +44,45 @@ bool operator==( const move& o, const move& a )
 
 struct move_packed
 {
-  unsigned char data[6];
+    move_packed( const move& m )
+    {
+        piece = (unsigned)m.piece;
+        captured = (unsigned)m.captured;
+        promoted = (unsigned)m.promoted;
+        from = (unsigned)m.from;
+        to = (unsigned)m.to;
+        type = (unsigned)m.type;
+        special = (unsigned)m.special;
+    }
+    move_packed():
+        piece   (0),   
+        captured(0),
+        promoted(0),
+        from    (0),
+        to      (0),
+        type    (0),
+        special (0){}
+
+    unsigned piece    : 4;
+    unsigned captured : 4;
+    unsigned promoted : 4;
+    unsigned from     : 6;
+    unsigned to       : 6;
+    unsigned type     : 1;
+    unsigned special  : 2;
 };
 
+move MoveFromMovePacked( const move_packed& mp )
+{
+    move m;
+    m.piece = (PieceType::EPieceType)(mp.piece);
+    m.captured = (PieceType::EPieceType)(mp.captured);
+    m.promoted = (PieceType::EPieceType)(mp.promoted);
+    m.from = (Sq::ESq)(mp.from);
+    m.to = (Sq::ESq)(mp.to);
+    m.type = (MoveType::EMoveType)(mp.type);
+    m.special = (MoveType::ESpecialMoveType)(mp.special);
+    return m;
+}
 
 #endif
