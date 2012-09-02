31555ed5c8e091c72390ca56c002719192ed53ef
Akanksh Vashisth <akankshvashisth@users.noreply.github.com>
2012-09-02 21:26:30 +0000

    minor reworks

--- typedefs.hpp
+++ typedefs.hpp
@@ -1,6 +1,8 @@
 #ifndef __typedefs_hpp
 #define __typedefs_hpp
 
+
+#ifdef HAVE_BOOST
 #include <boost/cstdint.hpp>
 
 typedef boost::uint8_t  ui08;
@@ -13,6 +15,20 @@ typedef boost::int32_t i32;
 typedef boost::int64_t i64;
 typedef ui64 BB; //bitboard
 typedef ui64 C64;
+#else
 
+#include <cstdint>
+
+typedef uint8_t  ui08;
+typedef uint16_t ui16;
+typedef uint32_t ui32;
+typedef uint64_t ui64;
+typedef int8_t  i08;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef ui64 BB; //bitboard
+typedef ui64 C64;
+#endif
 #endif
 
